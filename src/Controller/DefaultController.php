1142637e6adbcf75bf62784bd15498e2f023bccd
yasmina-dev <yasmina.ikhlef91@gmail.com>
2020-07-04 13:54:39 +0200

    change name path titi

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/toto", name="default")
+     * @Route("/tata", name="default_tata")
      */
     public function index()
     {
