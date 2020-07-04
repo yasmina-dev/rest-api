1a3bc230218413fcbf5309fb5a1ef1500f43efb5
mourad <chabour.mourad87@gmail.com>
2020-07-04 13:03:18 +0200

    modifier le path de la route

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="default")
+     * @Route("/default", name="default")
      */
     public function index()
     {
